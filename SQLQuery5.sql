538ca1140037bc1b92fc9750e14d394fa103139f
Remi Rodrigues Neto <112720074+RemiNeto@users.noreply.github.com>
2023-04-04 23:25:57 -0300

    Add files via upload

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -1,5 +1,5 @@
 SELECT * FROM Person.Address;
-SELECT * FROM SALES.SalesOrderDetail;
+SELECT * FROM Sales.SalesOrderDetail;
 SELECT * FROM person.Person;
 SELECT * FROM HumanResources.Employee;
 SELECT * FROM Person.EmailAddress;
@@ -52,12 +52,49 @@ SELECT ProductID, COUNT(ProductID) AS 'QUANTIDADE', AVG(OrderQty) AS 'MÉDIA' FRO
 /*DESAFIO 21*/
 SELECT StateProvinceID, COUNT(StateProvinceID) AS 'QUANTIDADE' FROM Person.Address GROUP BY StateProvinceID HAVING COUNT(StateProvinceID) > 1000;
 /*DESAFIO 22*/
-SELECT * FROM SALES.SalesOrderDetail;
+SELECT ProductID, AVG(LineTotal) AS 'VENDAS' FROM SALES.SalesOrderDetail GROUP BY ProductID HAVING AVG(LineTotal) < 1000000 ORDER BY ProductID;
 /*DESAFIO 23*/
+SELECT FirstName AS "PRIMEIRO NOME", LastName AS "SOBRENOME" FROM person.Person;
 /*DESAFIO 24*/
+SELECT ProductNumber AS "NÚMERO DO PRODUTO" FROM Production.Product;
 /*DESAFIO 25*/
+SELECT UnitPrice AS "PREÇO UNITÁRIO" FROM Sales.SalesOrderDetail;
 /*DESAFIO 26*/
+SELECT P.ListPrice, P.Name, PC.Name 
+FROM Production.Product P 
+INNER JOIN Production.ProductSubcategory PC ON PC.ProductCategoryID = P.ProductSubcategoryID
 /*DESAFIO 27*/
+SELECT PN.PhoneNumberTypeID, PN.Name, PP.BusinessEntityID, PP.PhoneNumber
+FROM Person.PhoneNumberType PN 
+INNER JOIN Person.PersonPhone PP ON PP.PhoneNumberTypeID = PN.PhoneNumberTypeID
+ORDER BY PP.BusinessEntityID
 /*DESAFIO 28*/
+SELECT PS.StateProvinceID,  PA.AddressID, PS.Name, PA.City
+FROM Person.StateProvince PS
+INNER JOIN Person.Address PA ON PA.StateProvinceID = PS.StateProvinceID
+ORDER BY PA.StateProvinceID
 /*DESAFIO 29*/
+SELECT *
+FROM Person.Address
+WHERE StateProvinceID IN 
+(SELECT StateProvinceID FROM Person.StateProvince WHERE Name = 'ALBERTA')
 /*DESAFIO 30*/
+SELECT ProductID, DATEPART(MONTH,StartDate) AS MÊS
+FROM Production.WorkOrder
+/*DESAFIO 31*/
+SELECT CONCAT(FirstName,' ', LastName) AS 'NOME COMPLETO',
+UPPER(FirstName) AS 'MAIÚSCULA',
+LOWER(LastName) AS 'MINÚSCULA',
+LEN(CONCAT(FirstName,' ', LastName)) AS TAMANHO,
+SUBSTRING(FirstName,1,3) AS ABREVIAÇÃO
+FROM Person.Person
+/*DESAFIO 32*/
+SELECT ProductNumber, REPLACE(ProductNumber, '-', '_') AS MUDAR
+FROM Production.Product
+/*DESAFIO 33*/
+SELECT MIN(LineTotal) AS MENOR, MAX(LineTotal) AS MAIOR, AVG(LineTotal) AS MÉDIA
+FROM Sales.SalesOrderDetail
+/*DESAFIO 34*/
+/*DESAFIO 35*/
+/*DESAFIO 36*/
+/*DESAFIO 37*/
